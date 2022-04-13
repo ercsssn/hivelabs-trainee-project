13281e255737f5049051bd30276ca0c2d110049e
John Ericsson Robarios <ercsssn@gmail.com>
2022-04-13 15:31:56 +0800

    Incorporated company logo and colors

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,8 +32,8 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                <div class="sidebar-brand-icon">
+                    <img src="{{asset('img/hivelabs_favicon.png')}}" alt="hivelabs icon">
                 </div>
                 <div class="sidebar-brand-text mx-3">Hive Admin <sup>2</sup></div>
             </a>
