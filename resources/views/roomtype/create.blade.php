5d8c6791e0d1205365de125d499f67d8df8d8bd6
ercsssn <ercsssn@gmail.com>
2022-04-15 16:21:51 +0800

    Create Add New and View All buttons
    These buttons bridge the create and index view

--- resources/views/roomtype/create.blade.php
+++ resources/views/roomtype/create.blade.php
@@ -6,7 +6,9 @@
    <!-- DataTales Example -->
    <div class="card shadow mb-4">
        <div class="card-header py-3">
-           <h6 class="m-0 font-weight-bold text-primary">Add Room Type</h6>
+           <h6 class="m-0 font-weight-bold text-primary">Add Room Type
+            <a href="{{ url('admin/roomtype') }}" class="float-right btn btn-success btn-sm">View All</a>
+           </h6>
        </div>
        <div class="card-body">
            <div class="table-responsive">
--- resources/views/roomtype/index.blade.php
+++ resources/views/roomtype/index.blade.php
@@ -6,7 +6,9 @@
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Room Types</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Room Types
+                <a href="{{ url('admin/roomtype/create') }}" class="float-right btn btn-success btn-sm">Add New</a>
+            </h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
