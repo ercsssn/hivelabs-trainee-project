d0361943e5fb3e468b00aa8ed608139add9f4fa8
John Ericsson Robarios <ercsssn@gmail.com>
2022-04-18 14:48:14 +0800

    Create Customer model and migrations

---
+++ database/migrations/2022_04_18_061146_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('mobile_number');
+            $table->string('permanent_address');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
