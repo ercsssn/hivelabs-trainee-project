8fa6f9174c34ea71365cca450ad67356f72c7f9b
John Ericsson Robarios <ercsssn@gmail.com>
2022-04-19 17:44:28 +0800

    Create login and check_login methods

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Admin;
+
+class AdminController extends Controller
+{
+    //Login
+    function login() 
+    {
+        return view('login');
+    }
+
+    //Check Login
+    function check_login(Request $request)
+    {
+        $request->validate([
+            'username'=>'required',
+            'password'=>'required',
+        ]);
+
+        $admin = Admin::where(['username'=>$request->username, 'password'=>sha1($request->password)])->count();
+
+        if ($admin > 0) {
+            $adminData = Admin::where(['username'=>$request->username, 'password'=>sha1($request->password)])->get();
+            session(['adminData'=>$adminData]);
+            return redirect('admin');
+        }else{
+            return redirect('admin/login')->with('msg','Invalid Username or Password!');
+        }
+
+
+    }
+    //Logout
+}
