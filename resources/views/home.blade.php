35b124ba9bb59e96807f73d1aed86541ebb66345
John Ericsson Robarios <ercsssn@gmail.com>
2022-04-19 16:49:00 +0800

    Create login view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
+    <link rel="shortcut icon" href="{{asset('img/hivelabs_favicon.png')}}">
     <link rel="stylesheet" href="{{asset('bs5/bootstrap.min.css')}}">
 </head>
 <body>
