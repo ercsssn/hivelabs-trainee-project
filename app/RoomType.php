064534bea0b12db68f99c6ecdd7ed78bd1da08e0
John Ericsson Robarios <ercsssn@gmail.com>
2022-04-20 10:19:56 +0800

    Define model relation
    hasMany relation to access RoomTypeImage from RoomType.

--- app/RoomType.php
+++ app/RoomType.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoomType extends Model
 {
-    //
+    function roomtypeimgs() 
+    {
+        return this->hasMany(RoomTypeImage::class,'room_type-id');
+    }
 }
