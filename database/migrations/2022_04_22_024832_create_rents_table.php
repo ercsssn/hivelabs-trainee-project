83f59c58ecfe6e6b4348f6d8b9de72a38b3eff52
John Ericsson Robarios <ercsssn@gmail.com>
2022-04-22 10:50:21 +0800

    Create Rent model and migrations

---
+++ database/migrations/2022_04_22_024832_create_rents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('tenant_id');
+            $table->integer('room_id');
+            $table->string('check_in_date');
+            $table->string('check_out_date');
+            $table->string('total_adults');
+            $table->string('total_children');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rents');
+    }
+}
