137b86ef5502ae8c4bc72e6cd511865a6d46ef8e
John Ericsson Robarios <ercsssn@gmail.com>
2022-04-22 11:00:01 +0800

    Define relation between Rent to tenant and room

--- app/Rent.php
+++ app/Rent.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rent extends Model
 {
-    //
+    function tenant() 
+    {
+        return $this->belongsTo(Tenant:class);
+    }
+
+    function room() 
+    {
+        return $this->belongsTo(Room:class);
+    }
 }
