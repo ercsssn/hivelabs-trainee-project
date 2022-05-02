182934539c3f18eabda80a617332326833a45d48
John Ericsson Robarios <ercsssn@gmail.com>
2022-05-02 17:55:53 +0800

    Include app.js

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Project: AMS</title>
     <link rel="shortcut icon" href="{{asset('img/hivelabs_favicon.png')}}">
     <link rel="stylesheet" href="{{asset('bs5/bootstrap.min.css')}}">
@@ -60,5 +61,7 @@
         </footer>
       </div>
       <!-- End of Footer -->
+
+      <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
