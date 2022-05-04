1659ec9aeea00f4ec529cb74115f08b1529dd78d
John Ericsson Robarios <ercsssn@gmail.com>
2022-05-04 10:57:31 +0800

    Set Channel at app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,4 +29,10 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    created() {
+        Echo.channel('room-added')
+        .listen('NewRoomAddition', (e)=>{
+            alert('GGWP, alaws refresh refresh!');
+        });
+    }
 });
