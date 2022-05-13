8c54dfcd36706aa6e5e4526e34ef30bc4b7ab309
John Ericsson Robarios <ercsssn@gmail.com>
2022-05-13 15:54:09 +0800

    Place the Dashboard heading to topbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -214,6 +214,11 @@
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
+                     <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between">
+                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                    </div>
+
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
